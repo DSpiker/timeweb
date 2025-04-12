b1e022e8b94188ae7d4311ad9b1746858687a568
DSpiker <dspiker23@gmail.com>
2025-04-12 16:24:40 -0500

    moved things over

---
+++ test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+
+    return app.test_client()
+
+def test_time_format(client):
+    response = client.get('/')
+
+    assert response.status_code == 200
+
+    assert 'Current time:' in response.get_data(as_text=True)
+
