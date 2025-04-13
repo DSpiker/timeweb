4adcdd19749498b26d13545ba452dad9910f1bad
DSpiker <dspiker23@gmail.com>
2025-04-12 23:30:21 -0500

    s

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-git config --global user.name "$GITHUB_USER"
-git config --global user.email "$GITHUB_EMAIL"
+set -e
+
+git config --global user.name "${GITHUB_USER}"
+git config --global user.email "${GITHUB_EMAIL}"
 
 if [ ! -d "/home/developer/timeweb" ]; then
-  git clone https://$GITHUB_TOKEN@github.com/DSpiker/timeweb /home/developer/timeweb
+  git clone "https://${GITHUB_TOKEN}@github.com/DSpiker/timeweb" /home/developer/timeweb
 fi
 
-exec bash
+cd /home/developer/timeweb
+
+exec /bin/bash
 
